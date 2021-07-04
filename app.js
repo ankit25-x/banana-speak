7e68ab16c2c47122c147d3b849aa1c79e8c351ae
ankit25-x <ankitgupta3225@gmail.com>
2021-07-05 00:39:34 +0530

    feat
    updated code to get minion translation output

--- app.js
+++ app.js
@@ -4,9 +4,23 @@ var txtInput = document.querySelector("#txt-input");
 
 var outputDiv = document.querySelector("#output");
 
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(text){
+    return serverURL + "?" + "text=" + text
+}
 
 function clickEventHandler(){
-    outputDiv.innerText = "sfetggvdferg  " + txtInput.value;
+    var inputText = txtInput.value;
+
+    fetch(getTranslationURL(inputText))
+      .then(response => response.json())
+      .then(json => {
+          var translatedText = json.contents.translated;
+          outputDiv.innerText = translatedText;
+      })
+
+
 };
 
 
