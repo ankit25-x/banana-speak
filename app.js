144dda6c109bc64062c9c3de423f49b7ecaccd49
ankit25-x <ankitgupta3225@gmail.com>
2021-08-21 19:46:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ var txtInput = document.querySelector("#txt-input");
 
 var outputDiv = document.querySelector("#output");
 
-var serverURL = "https://api.funtranslations.com/translate/minion.json"
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
 function getTranslationURL(text){
-    return serverURL + "?" + "text=" + text
+    return serverURL + "?" + "text=" + text;
 }
 
 function clickEventHandler(){
